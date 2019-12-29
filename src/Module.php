c4ed28a7aa88a6b5fb129f981bb46a52506b9be5
Judzhin Miles <info@msbios.com>
2019-12-29 12:33:07 +0200

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -16,7 +16,7 @@ use Zend\Stdlib\ArrayUtils;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.7';
 
     /** @const ENABLED */
     const ENABLED = 'enabled';
